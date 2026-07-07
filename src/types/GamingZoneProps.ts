6c6eb3ca10ecaf26e1e7ab49c471c76b65163fed
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-07-07 12:27:38 +0200

    feat: GamingZoneProps added

---
+++ src/types/GamingZoneProps.ts
@@ -0,0 +1,12 @@
+// Gaming zone must show dynamically: 
+    // Hearts remaining
+    // Mystery word
+    // The color of the letters tried (red if false for example)
+    // Send to the back the letter clicked.
+
+export type GamingZoneProps = {
+    hearts: number;
+    mysteryWord: string;
+    colorLetter: Record<string, boolean>;
+    onLetterClick: (letter: string) => void;
+}
--- src/types/HeaderProps.ts
+++ src/types/HeaderProps.ts
@@ -1,4 +1,8 @@
+// Header must display two information :
+// A function to start a new game and increment the former number of games played
+// The current number of games played. 
+
 export type HeaderProps = {
-    onNewGame: () => void;
+    onNewGame: () => void; 
     gamesPlayed: number;
 }
