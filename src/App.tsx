1ee4a5d5bb2b112cc646e8f3534f9ae7ba537ff0
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-07-14 17:55:48 +0200

    feat: mysteryWord, colorLetter, isLoading, errorMessage statesand the fetchRandomWord function are placed now in useMysteryWord hook.

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,6 @@ function App() {
   // State for hearts remaining for each game
   const [hearts, setHearts] = useState(5);
 
-  // State for mystery word
-  const [mysteryWord, setMysteryWord] = useState("");
-  console.log(mysteryWord)
-
-  // State for loading
-  const [isLoading, setIsLoading] = useState(true);
-
-  // State for error message
-  const [errorMessage, setErrorMessage] = useState("");
-
-  // State for color letters
-  const [colorLetter, setColorLetter] = useState<Record<string, boolean>>({});
-
   // Function to reset the game
   const resetGame = () => {
 
@@ -67,50 +54,6 @@ function App() {
       return " "
     }
   });
-
-  const fetchRandomWord = async () => {
-
-    const API_URL = import.meta.env.VITE_API_URL;
-
-    // Headers content
-    const headersContent = { "Content-Type": "application/json" };
-
-    try {
-
-      // Consuming back API 
-      const response = await fetch(API_URL, {
-        method: "GET",
-        headers: headersContent
-      });
-
-      // Checking that the server answer is working
-      if (!response.ok) {
-        console.error("Erreur lors du fetch pour afficher un mot aléatoire");
-        setIsLoading(false);
-        setErrorMessage("Un problème est survenu lors du chargement du mot mystère.")
-        return;
-      };
-
-      // Data contain a object sent from the back { "id": 1, "name": "word"}
-      const data = await response.json();
-
-      // We select the key name that contains the word
-      const fetchedWord: string = data.name.toUpperCase();
-
-      setMysteryWord(fetchedWord);
-      setColorLetter({ [fetchedWord[0]]: true });
-      setIsLoading(false);
-
-      return;
-
-    } catch (error) {
-
-      console.error(error);
-      setIsLoading(false);
-      setErrorMessage("Impossible de joindre le serveur.")
-    };
-
-  };
 
   // Keydown event
   useEffect(() => {
--- src/hooks/useMysteryWord.ts
+++ src/hooks/useMysteryWord.ts
@@ -1,3 +1,62 @@
-export const useMysteryWord = () => { 
+import { useState } from "react";
+
+export const useMysteryWord = () => {
+
+    // State for mystery word
+    const [mysteryWord, setMysteryWord] = useState("");
+
+    // State for color letters
+    const [colorLetter, setColorLetter] = useState<Record<string, boolean>>({});
+
+    // State for loading
+    const [isLoading, setIsLoading] = useState(true);
+
+    // State for error message
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const fetchRandomWord = async () => {
+    
+        const API_URL = import.meta.env.VITE_API_URL;
+    
+        // Headers content
+        const headersContent = { "Content-Type": "application/json" };
+    
+        try {
+    
+          // Consuming back API 
+          const response = await fetch(API_URL, {
+            method: "GET",
+            headers: headersContent
+          });
+    
+          // Checking that the server answer is working
+          if (!response.ok) {
+            console.error("Erreur lors du fetch pour afficher un mot aléatoire");
+            setIsLoading(false);
+            setErrorMessage("Un problème est survenu lors du chargement du mot mystère.")
+            return;
+          };
+    
+          // Data contain a object sent from the back { "id": 1, "name": "word"}
+          const data = await response.json();
+    
+          // We select the key name that contains the word
+          const fetchedWord: string = data.name.toUpperCase();
+    
+          setMysteryWord(fetchedWord);
+          setColorLetter({ [fetchedWord[0]]: true });
+          setIsLoading(false);
+    
+          return;
+    
+        } catch (error) {
+    
+          console.error(error);
+          setIsLoading(false);
+          setErrorMessage("Impossible de joindre le serveur.")
+        };
+    
+      };
+
 
 }
